ea34d10147dd2c4aea0ca730dfb5a7a490ba58ec
Mehmet <302574757@student.rocmondriaan.nl>
2019-11-22 11:45:36 +0100

    test

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('article/show.html.twig', [
+            'title'=>"Why Astroids Taste Like Bacon",
+            'comments' => $comments,
+        ]);
+    }
+}
